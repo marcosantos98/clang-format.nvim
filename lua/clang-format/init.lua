1d635101669e0cd2c6af71798a2093b6e6420837
marcosantos98 <marcomsantos98@gmail.com>
2024-02-28 23:51:59 +0000

    Initial commit

---
+++ lua/clang-format/init.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.setup(opts)
+
+	opts = opts or {}
+
+	assert(opts.clangFormatPath, "Expected .clang-format path to be specified. Set clangFormatPath in opts.")
+
+	vim.api.nvim_create_autocmd({"BufWritePost"}, {
+		pattern = {"*.c", "*.h"},
+		callback = function(ev)
+			local out = vim.fn.system({"clang-format", "-i", ev.match, "--style=file:" .. opts.clangFormatPath})
+			print(out)
+			vim.cmd("e!")
+		end
+	})
+end
+
+return M
